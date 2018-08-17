7eba20342147a372590827f0de69e25c919480c6
assly <blue20will@gmail.com>
2018-08-17 16:54:05 +0900

    fix: Create kaede's contribution

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 new fullpage('#fullpage', {
+  sectionsColor: ['#000', '#ff9090', '#ffcc99',　'#99fd99', '#99ccff',　'#aabbcc'],
   parallax: true,
-  loopHorizontal: false,
+  loopHorizontal: true,
   loopBottom: true,
+  loopTop: true,
   recordHistory: false
 });
