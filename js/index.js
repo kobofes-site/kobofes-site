5e3522e7904be839511d537fa9a46bb5f7e07b9b
assly <blue20will@gmail.com>
2018-08-19 10:57:20 +0900

    add: Add Grid for BoothInfo section with purecss import

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
-new fullpage('#fullpage', {
-  sectionsColor: ['#000', '#ff9090', '#ffcc99',　'#99fd99', '#99ccff',　'#aabbcc'],
+/* eslint no-undef: 0, new-cap: 0, no-unused-vars: 0 */
+const fp = new fullpage('#fullpage', {
+  sectionsColor: ['#000', '#ff9090', '#ffcc99', '#99fd99', '#99ccff', '#aabbcc'],
   parallax: true,
   loopHorizontal: true,
-  loopBottom: true,
-  loopTop: true,
-  recordHistory: false
+  loopBottom: false,
+  loopTop: false,
+  recordHistory: false,
+  // On mobile, hide controlarrow will be better
+  // But on PC, this will be disable to scroll side page
+  // Plans:
+  //   Split web page; mobile, PC
+  // controlArrows: false,
 });
