a0629479790629f873bb32c4e340d1a4a8b3a523
assly <blue20will@gmail.com>
2018-08-23 21:49:38 +0900

    add: Move shop section to second's, move sponsor banner to bottom and modify colorings

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /* eslint no-undef: 0, new-cap: 0, no-unused-vars: 0 */
 const fp = new fullpage('#fullpage', {
-  sectionsColor: ['#2f2f2f', '#ffa5b1', '#fff79b', '#aaef91', '#00d5ff', '#aabbcc'],
+  sectionsColor: ['#2f2f2f', '#ffa5b1', '#fff79b', '#aaef91', '#00d5ff', '#aabbcc', '#dfdfdf'],
   parallax: true,
   loopHorizontal: true,
   loopBottom: false,
